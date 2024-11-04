521e63b17aed3c4c952e9e86caa3cd43078e3027
IVANFROL <frolovitch.ivan@mail.ru>
2024-11-05 00:22:11 +0300

    Исправление ошибок ESLint и обновление конфигурации

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import _ from "lodash";
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+import _ from 'lodash';
+import getBirthYear from './src/index.js';
 
-// file: index.js
-import getBirthYear from "./src/index.js";
+console.log('Hello, Hexlet!');
+console.log(_.last(['one', 'two']));
 
-console.log("Сколько вам лет?");
+console.log('Сколько вам лет?');
 const age = 18; // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
 const birthYear = getBirthYear(age);
 console.log(`Ваш год рождения: ${birthYear}`);
